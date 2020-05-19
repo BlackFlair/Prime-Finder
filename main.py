07c5bfe5925988729cad5c543e7ef6604b0ce0f7
BlackFlair <surinmachaiah@gmail.com>
2020-05-19 14:55:09 +0530

    check7

--- main.py
+++ main.py
@@ -44,7 +44,31 @@ def check5(num_list):
         return 0
 
 def check7(num_list):
-    pass
+    c = 0
+    pattern = ['1','3','2','6','4','5']
+    # rev_list = num_list.reverse()
+    # print(type(num_list.reverse()))
+    rev_list = []
+
+    # for q in range (len(num_list),0):
+    #     rev_list.append(int(num_list[q]))
+    #
+    # print(rev_list)
+
+    while True:
+        for i in range (0,len(rev_list)):
+            rev_list[i] = rev_list[i] * pattern[c]
+            c += 1
+            if c == 6:
+                c = 0
+
+        if sum(rev_list) < 100000:
+            if sum(rev_list) % 7 == 0:
+                return 1
+            else:
+                return 0
+
+        rev_list = rev_list.reverse()
 
 print(num_list)
 print("Computing...")
